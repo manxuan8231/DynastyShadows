a13378023d5bf5ef59e6fac93b0c46d7cc5565de
manxuan8231 <lydangxuanman05@gmail.com>
2025-05-15 00:03:25 +0700

    inventory

---
+++ Assets/ScenceMan/Inventory/OpenInventory.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+
+public class OpenInventory : MonoBehaviour
+{
+    public GameObject inventoryCanvas;
+
+    public GameObject panelStatus;
+    public GameObject panelInven;
+    public GameObject panelSkill;
+
+    public AudioSource audioSource;
+    public AudioClip audioClipClick;
+    void Start()
+    {
+        inventoryCanvas.SetActive(false);
+        panelStatus.SetActive(true);
+        panelInven.SetActive(false);
+        panelSkill.SetActive(false);
+        audioSource = GetComponent<AudioSource>();
+    }
+
+   
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab)) 
+        {
+            if (inventoryCanvas.activeSelf) 
+            {
+                inventoryCanvas.SetActive(false);
+                Time.timeScale = 1.0f;
+                // Ẩn chuột khi tắt map
+                Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.Locked;
+            } 
+            else 
+            {
+                inventoryCanvas.SetActive(true);
+                Time.timeScale = 0f;
+                // Hiện chuột khi mở map
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+            }
+        }
+    }
+    //status
+    public void OpenButtonStatus()
+    {
+        panelStatus.SetActive(true);
+        panelInven.SetActive(false);
+        panelSkill.SetActive(false);
+        audioSource.PlayOneShot(audioClipClick);
+    }
+    
+
+    //inven
+    public void OpenButtonInven()
+    {
+        panelInven.SetActive(true);
+        panelStatus.SetActive(false);
+        panelSkill.SetActive(false);
+        audioSource.PlayOneShot(audioClipClick);
+    }
+   
+
+    //skill
+    public void OpenButtonSkill()
+    {
+        panelSkill.SetActive(true);
+        panelInven.SetActive(false);
+        panelStatus.SetActive(false);
+        audioSource.PlayOneShot(audioClipClick);
+    }
+   
+}
