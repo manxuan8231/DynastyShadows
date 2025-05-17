987621d61636a769bdfcb4f63150668c26c0ea15
manxuan8231 <lydangxuanman05@gmail.com>
2025-05-17 18:17:42 +0700

    d

---
+++ Assets/Script/FlipCameraUI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class FlipCameraUI : MonoBehaviour
+{
+    private Camera mainCamera;
+    
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    
+    void Update()
+    {          
+        // Luôn quay mặt về camera
+        if (mainCamera != null)
+        {
+            transform.rotation = Quaternion.LookRotation(transform.position - mainCamera.transform.position);
+        }
+    }
+}
