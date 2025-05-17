92b726b50857b4e135ab6647dad7d56bc506b170
manxuan8231 <lydangxuanman05@gmail.com>
2025-05-17 23:04:41 +0700

    f

--- Assets/Script/LimitCamera.cs
+++ Assets/Script/LimitCamera.cs
@@ -4,6 +4,10 @@ public class LimitCamera : MonoBehaviour
 {
     public GameObject player;
     public float positionY = 30f;
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
     private void LateUpdate()
     {
         transform.position = new Vector3(player.transform.position.x, positionY, player.transform.position.z);
