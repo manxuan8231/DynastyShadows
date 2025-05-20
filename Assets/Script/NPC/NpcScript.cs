fe0d03e2cbf69b46839e40a8ee7d9ea60fa9b828
manxuan8231 <lydangxuanman05@gmail.com>
2025-05-20 17:50:31 +0700

    npc hoithoai  UI conten

--- Assets/Script/NPC/NpcScript.cs
+++ Assets/Script/NPC/NpcScript.cs
@@ -12,28 +12,57 @@ public class NPCScript : MonoBehaviour
     public string[] names; // Danh sách tên (ai đang nói)
     public string[] content; // Nội dung hội thoại
 
-    private Coroutine coroutine; //ttuc
+    private Coroutine coroutine; //tieep tục hội thoại
 
+    public GameObject buttonF; // Nút F để tương tác với NPC
+    private bool isContent = true;
+    private bool isButtonF = false; // Kiểm tra trạng thái của nút F
+    //tham chieu
+    PlayerController playerController; // Tham chiếu đến PlayerController
+    ComboAttack comboAttack; // Tham chiếu đến ComboAttack
     void Start()
     {
+        // Lấy tham chiếu đến PlayerController và ComboAttack
+        playerController = FindAnyObjectByType<PlayerController>();
+        comboAttack = FindAnyObjectByType<ComboAttack>();
+        // Ẩn panel và nút F khi bắt đầu
         NPCPanel.SetActive(false);
+        buttonF.SetActive(false); // Ẩn nút F khi bắt đầu
         NPCName.text = "";
         NPCContent.text = "";
     }
-
-    private void OnTriggerEnter(Collider other)
+    private void Update()
     {
-        if (other.gameObject.CompareTag("Player"))
-        {
+        if (Input.GetKeyDown(KeyCode.F) && isButtonF)
+        {   
+            comboAttack.enabled = false; // Vô hiệu hóa ComboAttack
+            playerController.isController = false; // Vô hiệu hóa PlayerController
+            playerController.animator.SetBool("isWalking", false); // Dừng hoạt động của nhân vật
+            playerController.animator.SetBool("isRunning", false); // Dừng hoạt động của nhân vật
+            //
             NPCPanel.SetActive(true);
             coroutine = StartCoroutine(ReadContent());
+            buttonF.SetActive(false); // Ẩn nút F khi bắt đầu hội thoại
+            isButtonF = false; // Đặt trạng thái hội thoại là false
+            isContent = false; // Đặt lại trạng thái hội thoại
         }
     }
-
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") && isContent)
+        {
+            buttonF.SetActive(true); // Hiện nút F khi vào vùng tương tác
+            isButtonF = true; // Đặt trạng thái hội thoại là true
+        }
+    }
+                                    
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            buttonF.SetActive(false); // Ẩn nút F khi ra khỏi vùng tương tác
+           
+            isButtonF = false; // Đặt trạng thái hội thoại là false
             NPCPanel.SetActive(false);
             if (coroutine != null)
             {
@@ -60,11 +89,15 @@ public class NPCScript : MonoBehaviour
 
         // Ẩn panel sau khi kết thúc hội thoại
         NPCPanel.SetActive(false);
+        //
+        playerController.isController = true; // Bật lại PlayerController
+        comboAttack.enabled = true; // Bật lại ComboAttack
     }
 
-    public void EndContent()
+    public void EndContent()// Kết thúc hội thoại
     {
         NPCPanel.SetActive(false);
+        
         if (coroutine != null)
         {
             StopCoroutine(coroutine);
