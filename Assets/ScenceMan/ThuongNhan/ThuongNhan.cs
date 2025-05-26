51e8ce46f14d2387054d261d31eb29223d4c9d02
manxuan8231 <lydangxuanman05@gmail.com>
2025-05-26 21:37:57 +0700

    chuc nag npc

---
+++ Assets/ScenceMan/ThuongNhan/ThuongNhan.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using TMPro;
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class ThuongNhan : MonoBehaviour
+{
+    public float waitTimeScene = 2f; 
+    public float distance = 20f;// Khoảng cách tối đa để tương tác với NPC
+    private bool isSceneActive = true; // Biến để kiểm tra xem scene đã được kích hoạt hay chưa
+    public Transform playerTransform;// Tham chiếu đến Transform của người chơi
+    public TextMeshProUGUI textHelp;
+    public CinemachineCamera scene1;//
+    
+    void Start()
+    {
+        scene1.Priority = 0;
+        playerTransform = GameObject.FindWithTag("Player").transform; // Lấy Transform của người chơi
+        
+    }
+
+   
+    void Update()
+    {
+        float dis = Vector3.Distance(transform.position, playerTransform.position);// Tính khoảng cách giữa NPC và người chơi
+        if(dis <= distance && isSceneActive == true)
+        {
+            isSceneActive = false;
+            StartCoroutine(WaitScene(waitTimeScene));// Nếu khoảng cách nhỏ hơn hoặc bằng distance, bắt đầu coroutine
+        }
+    }
+    private IEnumerator WaitScene(float amount)
+    {
+        textHelp.text = "Cứu";
+        textHelp.gameObject.SetActive(true);// Hiển thị text
+        yield return new WaitForSeconds(1f);// Chờ 1 giây để người chơi có thời gian đọc
+        textHelp.text = "Ai đó cứu tôi với";
+        yield return new WaitForSeconds(1f);// Chờ 1 giây để người chơi có thời gian đọc
+        scene1.Priority = 10;// Đặt priority của camera hiện tại
+        yield return new WaitForSeconds(amount);// Chờ 2 giây
+        scene1.Priority = 0;// Trả về priority ban đầu
+    }
+}
