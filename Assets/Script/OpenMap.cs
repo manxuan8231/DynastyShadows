7d232d4d2a4986595efdb3d4e7a928b4b0181557
manxuan8231 <lydangxuanman05@gmail.com>
2025-06-18 00:31:10 +0700

    fix ui turn on

--- Assets/Script/OpenMap.cs
+++ Assets/Script/OpenMap.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-
+using System.Collections.Generic;
 public class OpenMap : MonoBehaviour
 {
     public GameObject mapUIConten;
@@ -7,20 +7,33 @@ public class OpenMap : MonoBehaviour
     public AudioSource mapAudio;
     public AudioClip mapClip;
 
+    //-------------------------------- cái nut ở dưới phải false ms cho bật map
+    public GameObject panelSkillTree;//panel skill tree
+    public GameObject imageBGButtonInven;//image bg button inven
+    public GameObject EquipMentMenu;
+    public GameObject[] loadPanel;
+    //tham chieu
+    private PlayerStatus playerStatus;
     void Start()
-    {
-        mapUIBG.SetActive(false);
-        mapUIConten.SetActive(false);
-        mapAudio = GetComponent<AudioSource>();
+{
+    mapUIBG.SetActive(false);
+    mapUIConten.SetActive(false);
+    mapAudio = GetComponent<AudioSource>();
 
-        // Ẩn chuột lúc bắt đầu nếu cần
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
-    }
+    // Ẩn chuột lúc bắt đầu nếu cần
+    Cursor.visible = false;
+    Cursor.lockState = CursorLockMode.Locked;
+
+    playerStatus = FindAnyObjectByType<PlayerStatus>();
+
+   
+}
+
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.M))
+        if (Input.GetKeyDown(KeyCode.M) && playerStatus.currentHp > 0 && panelSkillTree.activeSelf == false 
+        && imageBGButtonInven.activeSelf == false && EquipMentMenu.activeSelf == false)
         {
             if (mapUIConten.activeSelf)
             {
@@ -44,6 +57,13 @@ public class OpenMap : MonoBehaviour
                 Cursor.visible = true;
                 Cursor.lockState = CursorLockMode.None;
             }
+        }
+        if(Input.GetKeyDown(KeyCode.Tab) || Input.GetKeyDown(KeyCode.T) || Input.GetKeyDown(KeyCode.E))
+        {
+                
+                mapUIConten.SetActive(false);
+                mapUIBG.SetActive(false);
+               
         }
     }
 }
