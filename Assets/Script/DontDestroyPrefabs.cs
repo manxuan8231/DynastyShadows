163c95b3411b2a8d5f536763308882725f6f4326
manxuan8231 <lydangxuanman05@gmail.com>
2025-06-25 14:31:07 +0700

    hideUI

--- Assets/Script/DontDestroyPrefabs.cs
+++ Assets/Script/DontDestroyPrefabs.cs
@@ -5,15 +5,45 @@ using UnityEngine.SceneManagement;
 public class DontDestroyPrefabs : MonoBehaviour
 {
     public GameObject playerPre;
-  
+    private PlayerControllerState player;
+    public bool isLoadScene = false;
+    private void Start()
+    {
+        player = FindAnyObjectByType<PlayerControllerState>();
+      
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha0) && isLoadScene)
+        {
+            //skill4
+            player.animator.runtimeAnimatorController = player.animatorDefauld; // Trở về animator mặc định
+            player.skill4Manager.isHibitedIcon = false; // cam skill icon
+            player.isSkinSkill3Clone = false;
+            //skill2
+            player.skill2Manager.isHibitedIcon = false; // Bỏ cấm sử dụng skill 2
+            player.ChangeState(new PlayerCurrentState(player)); // Trở về trạng thái hiện tại
 
+            DontDestroyOnLoad(playerPre);
+            SceneManager.sceneLoaded += OnScenceLoad;
+            SceneManager.LoadScene("Map2");
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
         {
-          DontDestroyOnLoad(playerPre);
+            //skill4
+            player.animator.runtimeAnimatorController = player.animatorDefauld; // Trở về animator mặc định
+            player.skill4Manager.isHibitedIcon = false; // cam skill icon
+            player.isSkinSkill3Clone = false;
+            //skill2
+            player.skill2Manager.isHibitedIcon = false; // Bỏ cấm sử dụng skill 2
+            player.ChangeState(new PlayerCurrentState(player)); // Trở về trạng thái hiện tại
+
+            DontDestroyOnLoad(playerPre);
             SceneManager.sceneLoaded += OnScenceLoad;
-          SceneManager.LoadScene("Map2");
+            SceneManager.LoadScene("Map2");
         }
     }
     public void OnScenceLoad(Scene scene, LoadSceneMode mode)
---
+++ Assets/Script/SkillTree/HideUI.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class HideUI : MonoBehaviour
+{
+    public GameObject[] skillTreeUI;
+    public PlayerControllerState playerControllerState;
+    void Start()
+    {
+        playerControllerState = FindAnyObjectByType<PlayerControllerState>();
+    }
+
+   
+    void Update()
+    {
+        //ẩn UI skill tree khi animator bị tắt
+        if (playerControllerState.animator.enabled == false)
+        {
+            foreach (GameObject ui in skillTreeUI)
+            {
+                ui.SetActive(false);
+            }
+        }
+        else
+        {
+            foreach (GameObject ui in skillTreeUI)
+            {
+                ui.SetActive(true);
+            }
+        }
+    }
+}
