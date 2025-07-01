ee13639be86d323261460c09f50bff477efe5ae6
Minhtung0510 <169137996+Minhtung0510@users.noreply.github.com>
2025-07-02 02:31:34 +0700

    timeline quest 4

---
+++ Assets/ScenceTung/QuestMap2/Quest4/TimeLineBossDragonDead.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimeLineBossDragonDead : MonoBehaviour
+{
+    public GameObject timeLine;
+    public GameObject playerInGame; // Player gameplay
+    public GameObject playerTimeLine; // Player trong cutscene
+    public GameObject boss;
+    public PlayableDirector playableDirector;
+    void Start()
+    {
+        if (playerInGame == null)
+            playerInGame = GameObject.FindWithTag("Player"); // tìm theo tag cho dễ quản lý
+    }
+
+    public void Run()
+    {
+        
+            // Bắt đầu cutscene
+            Debug.Log("Kích hoạt timeline boss dead");
+            playerInGame.SetActive(false); // Ẩn player thật
+            timeLine.SetActive(true); // Bật timeline
+            playableDirector.Play(); // Chạy timeline
+            playableDirector.stopped += OnTimelineFinished; // Đăng ký sự kiện khi timeline kết thúc
+
+    }
+    private void OnTimelineFinished(PlayableDirector director)
+    {
+        // Cập nhật vị trí player thật từ player timeline
+        playerInGame.transform.position = playerTimeLine.transform.position;
+        playerInGame.transform.rotation = playerTimeLine.transform.rotation;
+        // Kết thúc cutscene, chơi tiếp
+        playerInGame.SetActive(true);
+        playerTimeLine.SetActive(false);
+        gameObject.SetActive(false); // Ẩn đối tượng sau khi timeline kết thúc
+        boss.SetActive(false); // Kích hoạt boss sau khi timeline kết thúc
+    }
+}
