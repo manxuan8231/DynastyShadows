ced7b66436fc3f7e486acf58e2b83c9cb9e7737d
PNGPhong <phongpngps40540@gmail.com>
2025-07-15 01:27:13 +0700

    XongSetting

--- Assets/ScenePhong/Script/PerformanceSettings.cs
+++ Assets/ScenePhong/Script/PerformanceSettings.cs
@@ -5,6 +5,5 @@ public class PerformanceSettings : MonoBehaviour
     void Start()
     {
         QualitySettings.vSyncCount = 0; // Tắt VSync
-        Application.targetFrameRate = 120; // Hoặc bạn có thể đặt thành 300, tùy game
     }
 }
--- Assets/ScenePhong/Script/SettingsManager.cs
+++ Assets/ScenePhong/Script/SettingsManager.cs
@@ -1,6 +1,8 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Audio;
+using TMPro;
+using System.Collections.Generic;
 
 public class SettingsManager : MonoBehaviour
 {
@@ -10,13 +12,31 @@ public class SettingsManager : MonoBehaviour
     [SerializeField] private Slider sfxSlider;
 
     [Header("FPS Settings")]
-    [SerializeField] private Dropdown fpsDropdown;
+    [SerializeField] private TMP_Dropdown fpsDropdown;
     private readonly int[] fpsOptions = { 60, 90, 120 };
 
+    public static SettingsManager Instance;
     void Start()
     {
         LoadVolumeSettings();
         LoadFPSSettings();
+        LoadVSyncSettings();
+        LoadResolutionSettings();
+        LoadQualitySettings();
+        LoadDisplayModeSetting();
+    }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject); // Ngăn tạo trùng
+        }
     }
 
     #region Volume
@@ -71,4 +91,192 @@ public class SettingsManager : MonoBehaviour
         Application.targetFrameRate = fpsOptions[index];
     }
     #endregion
+
+    #region VSync
+
+    [Header("VSync Settings")]
+    [SerializeField] private Toggle vsyncToggle;
+
+    public void OnVSyncToggleChanged(bool isOn)
+    {
+        QualitySettings.vSyncCount = isOn ? 1 : 0;
+        PlayerPrefs.SetInt("VSyncEnabled", isOn ? 1 : 0);
+    }
+
+    private void LoadVSyncSettings()
+    {
+        bool isVSyncOn = PlayerPrefs.GetInt("VSyncEnabled", 1) == 1; // Mặc định bật
+        vsyncToggle.isOn = isVSyncOn;
+        QualitySettings.vSyncCount = isVSyncOn ? 1 : 0;
+        vsyncToggle.onValueChanged.AddListener(OnVSyncToggleChanged);
+    }
+
+    #endregion
+
+    #region Display Mode
+
+    [SerializeField] private TMP_Dropdown displayModeDropdown;
+
+    private readonly string[] displayModes = { "Đầy màn", "Cửa sổ", "không viền" };
+
+    private void LoadDisplayModeSetting()
+    {
+        int savedIndex = PlayerPrefs.GetInt("DisplayModeIndex", 0);
+
+        displayModeDropdown.ClearOptions();
+        displayModeDropdown.AddOptions(new List<string>(displayModes));
+
+        displayModeDropdown.value = savedIndex;
+        displayModeDropdown.RefreshShownValue();
+
+        displayModeDropdown.onValueChanged.AddListener(OnDisplayModeChanged);
+
+        ApplyDisplayMode(savedIndex);
+    }
+
+    private void OnDisplayModeChanged(int index)
+    {
+        ApplyDisplayMode(index);
+        PlayerPrefs.SetInt("DisplayModeIndex", index);
+    }
+
+    private void ApplyDisplayMode(int index)
+    {
+        switch (index)
+        {
+            case 0: // Fullscreen
+                Screen.fullScreenMode = FullScreenMode.ExclusiveFullScreen;
+                break;
+            case 1: // Windowed
+                Screen.fullScreenMode = FullScreenMode.Windowed;
+                break;
+            case 2: // Borderless
+                Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
+                break;
+            default:
+                Screen.fullScreenMode = FullScreenMode.ExclusiveFullScreen;
+                break;
+        }
+    }
+
+    #endregion
+
+    #region Resolution & Quality
+
+    [Header("Resolution & Quality Settings")]
+    [SerializeField] private TMP_Dropdown resolutionDropdown;
+    [SerializeField] private TMP_Dropdown qualityDropdown;
+
+    private List<Resolution> uniqueResolutions = new List<Resolution>();
+    private Resolution[] resolutions;
+
+    private void LoadResolutionSettings()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+        List<Resolution> uniqueResolutions = new List<Resolution>();
+
+        HashSet<string> seen = new HashSet<string>();
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string resolutionText = resolutions[i].width + " x " + resolutions[i].height;
+
+            if (!seen.Contains(resolutionText))
+            {
+                seen.Add(resolutionText);
+                uniqueResolutions.Add(resolutions[i]);
+                options.Add(resolutionText);
+
+                if (resolutions[i].width == Screen.currentResolution.width &&
+                    resolutions[i].height == Screen.currentResolution.height)
+                {
+                    currentResolutionIndex = uniqueResolutions.Count - 1;
+                }
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+
+        int savedIndex = PlayerPrefs.GetInt("ResolutionIndex", currentResolutionIndex);
+        resolutionDropdown.value = savedIndex;
+        resolutionDropdown.RefreshShownValue();
+
+        resolutionDropdown.onValueChanged.AddListener((index) =>
+        {
+            Resolution selectedRes = uniqueResolutions[index];
+            Screen.SetResolution(selectedRes.width, selectedRes.height, Screen.fullScreen);
+            PlayerPrefs.SetInt("ResolutionIndex", index);
+        });
+    }
+
+
+
+    public void OnResolutionChanged(int index)
+    {
+        if (index >= 0 && index < uniqueResolutions.Count)
+        {
+            Resolution selected = uniqueResolutions[index];
+            Screen.SetResolution(selected.width, selected.height, Screen.fullScreen);
+            PlayerPrefs.SetInt("ResolutionIndex", index);
+        }
+    }
+
+    private void LoadQualitySettings()
+    {
+        qualityDropdown.ClearOptions();
+        string[] englishNames = QualitySettings.names;
+
+        List<string> vietnameseNames = new List<string>();
+
+        foreach (string name in englishNames)
+        {
+            switch (name.ToLower())
+            {
+                case "very low":
+                    vietnameseNames.Add("Rất thấp");
+                    break;
+                case "low":
+                    vietnameseNames.Add("Thấp");
+                    break;
+                case "medium":
+                    vietnameseNames.Add("Trung bình");
+                    break;
+                case "high":
+                    vietnameseNames.Add("Cao");
+                    break;
+                case "very high":
+                    vietnameseNames.Add("Rất cao");
+                    break;
+                case "ultra":
+                    vietnameseNames.Add("Cực đại");
+                    break;
+                default:
+                    vietnameseNames.Add(name); // fallback nếu có thêm mức khác
+                    break;
+            }
+        }
+
+        qualityDropdown.AddOptions(vietnameseNames);
+
+        int savedIndex = PlayerPrefs.GetInt("QualityLevel", QualitySettings.GetQualityLevel());
+        qualityDropdown.value = savedIndex;
+        qualityDropdown.RefreshShownValue();
+
+        qualityDropdown.onValueChanged.AddListener(OnQualityChanged);
+        QualitySettings.SetQualityLevel(savedIndex);
+    }
+
+    public void OnQualityChanged(int index)
+    {
+        QualitySettings.SetQualityLevel(index);
+        PlayerPrefs.SetInt("QualityLevel", index);
+    }
+
+    #endregion
+
+
 }
